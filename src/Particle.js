16059a29160fdd702fc1fbf81c1e6f8deaffa352
Grey <memor37@onet.pl>
2021-10-29 18:29:47 +0200

    Added speed, generationTime and lifeSpan properties

--- src/Particle.js
+++ src/Particle.js
@@ -1,10 +1,13 @@
 class Particle
 {
-    constructor(x, y, scale)
+    constructor(x, y, scale, generationTime)
     {
         this.x = x;
         this.y = y;
         this.scale = scale;
+        this.speed = 1;
+        this.generationTime = generationTime;
+        this.lifeSpan = 1
         this.size = 8;
         this.imageData = null;
     }
